c0367b73f0d0efb33addfc3bc96cab429948ccff
chirv <njchiaravalle@gmail.com>
2025-10-05 10:18:17 -0400

    simple classic agency webpage

---
+++ agency-starter-cl/script.js
@@ -0,0 +1,19 @@
+
+/*
+SPDX-License-Identifier: MPL-2.0
+Copyright (c) 2025 Nick Chiaravalle
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+// Smooth scroll for in-page nav (classic, tiny)
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', (e) => {
+    const id = a.getAttribute('href');
+    const target = document.querySelector(id);
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  });
+});
